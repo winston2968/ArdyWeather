3f6462f36425912c37c956b19c336d66701eed86
Winston2968 <winston2968@gmail.com>
2025-04-28 18:44:30 +0200

    Adding timer for datagram sendding

--- src/sensor_com/sensor_com.ino
+++ src/sensor_com/sensor_com.ino
@@ -1,112 +1,75 @@
-// ===========================================================================
-//                      Sensor Module Code
-// ===========================================================================
-
-/*
-  Communication Protocol
-*/
-
-// ==========================================================================
-// Include Libraries 
-
 #include "Arduino.h"
 #include "Wire.h"
-
 #include <TimerFour.h>
 #include <VirtualWire.h>
 
 int RF_TX_PIN = 4;  // Sending pin 
 
-// ------------ Global Variables ------------
-
-// Radio Protocol variables 
+// Variables globales
 static char SENSOR_NUM = '1';
-
-int nb_ack = 0;
-int nb_sent = 0;
+volatile int nb_ack = 0;
+volatile int nb_sent = 0;
+volatile bool doit_envoyer = false;  // <-- Un drapeau pour demander un envoi
 
 char datagram_type[] = {'A', 'W', '0', SENSOR_NUM, '0', '0', '0'}; 
+char datas_example[] = {'H','G','F','D','J','I','Z','J','C','K','J','B','Q','K','B','Z','S','B'};
 
-char datas_example[] = {'H','G','F','D','J','I','Z','J','C','K','J','B','Q','K','B','Z','S','B','V'};
+char datagram[27]; // <- Un seul datagram statique
 
-// ------------ Radio Protocol ------------
+// Fonction pour construire le datagram
+void build_datagram() {
+  if (nb_sent > 30) nb_sent = 0;
+  nb_sent += 1;
 
-/*
-  Datagram Structure : 
-    {Protocol, Target, Emmeteur, Type, Nb Send, Nb ACK, Datas}
-  Example : 
-    {AW, 2, 1, Datas, 10, 7, 6585689459076879544567980}
-  
-  - Protocol : Always AW, the name of the protocol. 
-  - Target : the number of target station. 
-  - Emmeteur : the number of actual station. 
-  - Type : 'A' or 'D' (acquitement or datas)
-  - Nb Send : Number of sent datagram. 
-  - Nb ACK : Number of acquitted datagram. 
-  - Datas : Datas if it's a Datas type datagram. 
-*/
+  memset(datagram, 0, 27);
 
-// Send datagram function 
-void send_data(char* msg) {
-  Serial.println("");
-  Serial.println("Sending message...");
-  for (int i = 0; i < 27; i++) {  // Affiche tout le message
-    Serial.print(msg[i]);
-  }
-  Serial.println("");
-  vw_send((uint8_t *)msg, 27); // Envoie 27 octets
-  vw_wait_tx();
-  Serial.println("Sent !");
-}
-
-// Build datagram message 
-char* build_datagram(char datas[], bool send_to_station) {
-  nb_sent += 1; 
-
-  // Initialiser le datagramme de 27 octets
-  char* datagram = (char*) malloc(27);  
-  memset(datagram, 0, 27);  // Initialiser avec des zéros
-
-  // Remplir les informations du header
-  datagram[0] = datagram_type[0];  // Protocole
+  datagram[0] = datagram_type[0];
   datagram[1] = datagram_type[1];
-  datagram[2] = '0';               // Station cible
-  datagram[4] = 'D';               // Type de datagramme
+  datagram[2] = '0'; // Target
+  datagram[4] = 'D'; // Type
   datagram[5] = '0' + nb_sent;
   datagram[6] = '0' + nb_ack;
 
-  // Remplir les données dans le datagramme (à partir du 7ème octet)
   for (int i = 7; i < 27; i++) {
-    datagram[i] = datas[i - 7];
+    datagram[i] = datas_example[i - 7];
   }
+}
 
-  return datagram;
+// Fonction appelée par l'interruption
+void on_timer() {
+  doit_envoyer = true;
 }
 
 void setup() {
   Serial.begin(9600);
-
   vw_set_tx_pin(RF_TX_PIN);
-  vw_setup(2000); // 2000 bits par seconde
+  vw_setup(2000);
+
+  Timer4.initialize(2000000); // 200 ms
+  Timer4.attachInterrupt(on_timer);
 }
 
 void loop() {
-  // Obtenir le datagramme
-  char* datagram = build_datagram(datas_example, true);
+  if (doit_envoyer) {
+    doit_envoyer = false; // reset le drapeau
 
-  // Afficher le datagramme créé
-  Serial.println("Datagram created : "); 
-  for (int i = 0 ; i < 27 ; i++) {
-    Serial.print(datagram[i]); 
+    build_datagram();
+
+    Serial.println("Datagram created:");
+    for (int i = 0; i < 27; i++) {
+      Serial.print(datagram[i]);
+    }
+    Serial.println("");
+
+    vw_send((uint8_t *)datagram, 27);
+    vw_wait_tx();
+
+    Serial.println("Sent!");
+    Serial.print("nb_sent: ");
+    Serial.println(nb_sent);
+
+    free(datagram);
+
+    Serial.println("");
   }
-  Serial.println("");
-
-  // Envoyer le datagramme via le signal radio
-  send_data(datagram);
-  Serial.println("");
-
-  // Libérer la mémoire après l'envoi
-  free(datagram);
-
-  delay(2000); // Délai de 2 secondes
 }
--- src/station_com/station_com.ino
+++ src/station_com/station_com.ino
@@ -3,25 +3,77 @@
 // ===========================================================================
 
 /*
-  Get temperature and humidity with GrooveSensor each delay int value. 
-  Send datas throught radio with VirtualWire.
+  Communication Protocol
 */
 
 // ==========================================================================
 // Include Libraries 
 
-// #include <TH06_dev.h>
+
 #include "Arduino.h"
 #include "Wire.h"
+// #include <TH06_dev.h>
 // #include "THSensor_base.h"
 
 #include <TimerFour.h>
 #include <VirtualWire.h>
 
-int RF_TX_PIN = 4;  // Sedding pin 
+int RF_TX_PIN = 4;  // Reception pin 
 
 
-// ------------ Global Variables ------------
+// ==========================================================================
+// Global Variables
+
+// Radio Protocol variables 
+static char STATION_NUM = '0';
+
+int nb_ack = 0;
+int nb_sent = 0;
+
+char datagram_type[] = {'A', 'W', '0', STATION_NUM, '0', '0', '0'}; 
+
+// Sensor Variables
+
+
+// ==========================================================================
+// Radio Protocol
+
+
+/*
+  Datagram Structure : 
+    {Protocol, Target, Emmeteur, Type, Nb Send, Nb ACK, Datas}
+  Example : 
+    {AW, 2, 1, Datas, 10, 7, 6585689459076879544567980}
+  
+  - Protocol : Always AW, the name of the protocol. 
+  - Target : the number of target station. 
+  - Emmeteur : the number of actual station. 
+  - Type : 'A' or 'D' (acquitement or datas)
+  - Nb Send : Number of sent datagram. 
+  - Nb ACK : Number of acquitted datagram. 
+  - Datas : Datas if it's a Datas type datagram. 
+*/
+
+// Send datagram function 
+void send_data(char* msg) {
+  Serial.println("");
+  Serial.println("Sending message...");
+  for (int i = 0; i < 27; i++) {  // Display all message 
+    Serial.print(msg[i]);
+  }
+  Serial.println("");
+  vw_send((uint8_t *)msg, 27); // Send 27 octets
+  vw_wait_tx();
+  Serial.println("Sent !");
+}
+
+// Create datagram function 
+
+
+
+// ==========================================================================
+// Setup and Loop
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,3 +84,22 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
